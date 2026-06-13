c2e485c0a0c4db001f225c036f5cc1cd10472ba8
Tri Lindu Aji <trilinduaji11@gmail.com>
2026-06-13 11:55:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<?php
+
+
+define('BASE_PATH', __DIR__);
+
+
+require_once BASE_PATH . '/core/App.php';
+App::init(BASE_PATH);
+
+require_once BASE_PATH . '/core/Router.php';
+require_once BASE_PATH . '/core/View.php';
+require_once BASE_PATH . '/core/helpers.php';
+require_once BASE_PATH . '/config/session.php';
+
+View::setPath(BASE_PATH . '/app/Views');
+
+
+require_once BASE_PATH . '/routes/web.php';
+
+
+$method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+$route  = trim($_GET['route'] ?? '', '/');
+
+Router::dispatch($method, $route);
 
